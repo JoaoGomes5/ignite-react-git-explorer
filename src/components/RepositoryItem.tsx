74e54e4cd28e6d98121c99c63d17d196d67e0b0e
Joao Gomes <joaopfg.2002@gmail.com>
2021-03-25 17:59:32 +0000

    end project

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,12 +1,21 @@
 
-function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+
+function RepositoryItem(props: RepositoryItemProps) {
   return (
     <>
        <li>
           <strong>{props.repository.name ?? 'default'}</strong>
           <p>{props.repository.description}</p>
 
-          <a href={props.repository.link}>
+          <a href={props.repository.html_url}>
             Go to repository
           </a>
         </li>
